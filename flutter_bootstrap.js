0e4476fc70666902d7e168974fee95f0fef8b5b0
Avadh Bambhroliya <avadhbambhroliya006@gmail.com>
2026-08-18 13:24:26 +0530

    Deploy Version 1.0.0+2 (2026-08-18 13:23:34)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "943178233" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1311703780" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
